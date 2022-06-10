6d35c19dd83480b245751e72bba485bc94fcd234
lsb1211 <105209968+lsb1211@users.noreply.github.com>
2022-06-10 14:18:59 +0900

    TestController 수정 by hong

--- demo/src/main/java/com/ez/demo/controller/TestController.java
+++ demo/src/main/java/com/ez/demo/controller/TestController.java
@@ -5,8 +5,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TestController {
 	public String test() {
-		System.out.println("test by hong");
-		
+		System.out.println("test by hong - TestController");
+		System.out.println("hong!!!!");
 		return "/index";
 	}
 }
